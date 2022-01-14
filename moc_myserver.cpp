9cfd617c31e61ab51ddf9a10316b8253a39571c6
Aleksey <90646243+AlekseyMelnik1@users.noreply.github.com>
2022-01-14 16:07:26 +0200

    Add files via upload
    Server

---
+++ moc_myserver.cpp
@@ -0,0 +1,133 @@
+/****************************************************************************
+** Meta object code from reading C++ file 'myserver.h'
+**
+** Created by: The Qt Meta Object Compiler version 67 (Qt 5.9.5)
+**
+** WARNING! All changes made in this file will be lost!
+*****************************************************************************/
+
+#include "../Server/myserver.h"
+#include <QtCore/qbytearray.h>
+#include <QtCore/qmetatype.h>
+#if !defined(Q_MOC_OUTPUT_REVISION)
+#error "The header file 'myserver.h' doesn't include <QObject>."
+#elif Q_MOC_OUTPUT_REVISION != 67
+#error "This file was generated using the moc from 5.9.5. It"
+#error "cannot be used with the include files from this version of Qt."
+#error "(The moc has changed too much.)"
+#endif
+
+QT_BEGIN_MOC_NAMESPACE
+QT_WARNING_PUSH
+QT_WARNING_DISABLE_DEPRECATED
+struct qt_meta_stringdata_MyServer_t {
+    QByteArrayData data[9];
+    char stringdata0[104];
+};
+#define QT_MOC_LITERAL(idx, ofs, len) \
+    Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
+    qptrdiff(offsetof(qt_meta_stringdata_MyServer_t, stringdata0) + ofs \
+        - idx * sizeof(QByteArrayData)) \
+    )
+static const qt_meta_stringdata_MyServer_t qt_meta_stringdata_MyServer = {
+    {
+QT_MOC_LITERAL(0, 0, 8), // "MyServer"
+QT_MOC_LITERAL(1, 9, 11), // "startServer"
+QT_MOC_LITERAL(2, 21, 0), // ""
+QT_MOC_LITERAL(3, 22, 18), // "incomingConnection"
+QT_MOC_LITERAL(4, 41, 7), // "qintptr"
+QT_MOC_LITERAL(5, 49, 16), // "socketDescriptor"
+QT_MOC_LITERAL(6, 66, 11), // "socketReady"
+QT_MOC_LITERAL(7, 78, 16), // "socketDisconnect"
+QT_MOC_LITERAL(8, 95, 8) // "SendFile"
+
+    },
+    "MyServer\0startServer\0\0incomingConnection\0"
+    "qintptr\0socketDescriptor\0socketReady\0"
+    "socketDisconnect\0SendFile"
+};
+#undef QT_MOC_LITERAL
+
+static const uint qt_meta_data_MyServer[] = {
+
+ // content:
+       7,       // revision
+       0,       // classname
+       0,    0, // classinfo
+       5,   14, // methods
+       0,    0, // properties
+       0,    0, // enums/sets
+       0,    0, // constructors
+       0,       // flags
+       0,       // signalCount
+
+ // slots: name, argc, parameters, tag, flags
+       1,    0,   39,    2, 0x0a /* Public */,
+       3,    1,   40,    2, 0x0a /* Public */,
+       6,    0,   43,    2, 0x0a /* Public */,
+       7,    0,   44,    2, 0x0a /* Public */,
+       8,    0,   45,    2, 0x0a /* Public */,
+
+ // slots: parameters
+    QMetaType::Void,
+    QMetaType::Void, 0x80000000 | 4,    5,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+
+       0        // eod
+};
+
+void MyServer::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+{
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        MyServer *_t = static_cast<MyServer *>(_o);
+        Q_UNUSED(_t)
+        switch (_id) {
+        case 0: _t->startServer(); break;
+        case 1: _t->incomingConnection((*reinterpret_cast< qintptr(*)>(_a[1]))); break;
+        case 2: _t->socketReady(); break;
+        case 3: _t->socketDisconnect(); break;
+        case 4: _t->SendFile(); break;
+        default: ;
+        }
+    }
+}
+
+const QMetaObject MyServer::staticMetaObject = {
+    { &QTcpServer::staticMetaObject, qt_meta_stringdata_MyServer.data,
+      qt_meta_data_MyServer,  qt_static_metacall, nullptr, nullptr}
+};
+
+
+const QMetaObject *MyServer::metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->dynamicMetaObject() : &staticMetaObject;
+}
+
+void *MyServer::qt_metacast(const char *_clname)
+{
+    if (!_clname) return nullptr;
+    if (!strcmp(_clname, qt_meta_stringdata_MyServer.stringdata0))
+        return static_cast<void*>(this);
+    return QTcpServer::qt_metacast(_clname);
+}
+
+int MyServer::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = QTcpServer::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 5)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 5;
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 5)
+            *reinterpret_cast<int*>(_a[0]) = -1;
+        _id -= 5;
+    }
+    return _id;
+}
+QT_WARNING_POP
+QT_END_MOC_NAMESPACE
